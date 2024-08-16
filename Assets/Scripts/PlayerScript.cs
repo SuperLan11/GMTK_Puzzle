f9ee70a4fcfbf140c5b9107a9f9466a4d87eb283
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2024-08-16 18:55:46 -0400

    got player and grab trigger to flip when turning

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,43 +7,60 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] GameObject playerObj;
     [SerializeField] float moveSpeed;
     [SerializeField] float grabRange;
+    [SerializeField] GameObject grabTrigger;
 
+    private bool facingRight;
     private bool isGrabbing;    
     private List<Transform> crateTransforms = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
+        facingRight = true;
         isGrabbing = false;
-        GameObject[] gameObjs = GameObject.FindObjectsOfType<GameObject>();        
+        GetCrates();
 
-        foreach(GameObject gameObj in gameObjs)
-        {
-            if(gameObj.name.Contains("Crate"))
-            {
-                crateTransforms.Add(gameObj.transform);
-            }
-        }
+        // make grabTriggerRenderer invisible
+        Renderer grabTriggerRenderer = grabTrigger.GetComponent<Renderer>();
+        //grabTriggerRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {        
+        // flip grab trigger to player direction...
+
         Vector3 newPosition = playerObj.transform.position;
+        Vector3 newTriggerPosition = grabTrigger.transform.position;
+
+        Renderer playerRenderer = GetComponent<Renderer>();      
 
         // move player right
         if (Input.GetKey(KeyCode.D))
         {
-            newPosition.x += (moveSpeed * Time.deltaTime);
+            newPosition.x += (moveSpeed * Time.deltaTime);            
             playerObj.transform.position = newPosition;
+
+            newTriggerPosition.x += (moveSpeed * Time.deltaTime);
+            // flip player and grab trigger when turning
+            if (!facingRight)            
+                playerObj.transform.eulerAngles = new Vector3(0, 0, 0);                     
+            facingRight = true;
         }
         // move player left
         else if(Input.GetKey(KeyCode.A))
         {
             newPosition.x -= (moveSpeed * Time.deltaTime);
             playerObj.transform.position = newPosition;
+
+            newTriggerPosition.x -= (moveSpeed * Time.deltaTime);
+            if (facingRight)            
+                playerObj.transform.eulerAngles = new Vector3(0, 180, 0);             
+            facingRight = false;
         }
-        
+
+        // when new levels are added, call GetCrates here on entering new level 
+
         // gets a crate in grab range, if any
         // does not account for multiple crates within range
         Transform grabbableCrate = GetGrabbableCrate();
@@ -59,16 +76,24 @@ public class PlayerScript : MonoBehaviour
                 // unparents crate from player
                 grabbableCrate.SetParent(null);
         }
+
+        // resize player and grabbed crate when shift pressed
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrabbing && grabbableCrate != null)
+        {
+            playerObj.transform.localScale *= 2;
+            grabbableCrate.transform.localScale *= 2;
+        }
     }
 
     private Transform GetGrabbableCrate()
     {
+        // get rid of transform list later
         foreach(Transform crateTransform in crateTransforms)
         {
             if (CrateInGrabRange(playerObj.transform, crateTransform))
                 return crateTransform;
         }
-        Debug.Log("Issue finding grabbable crates");
+        //Debug.Log("No crates in range");
         return null;
     }
 
@@ -111,5 +136,19 @@ public class PlayerScript : MonoBehaviour
         if (betweenDistance < grabRange)
             return true;
         return false;
+    }
+
+    private List<Transform> GetCrates()
+    {
+        GameObject[] gameObjs = GameObject.FindObjectsOfType<GameObject>();
+        crateTransforms.Clear();
+        foreach (GameObject gameObj in gameObjs)
+        {
+            if(gameObj.name.Contains("Crate"))
+            {
+                crateTransforms.Add(gameObj.transform);
+            }
+        }
+        return crateTransforms;
     }
 }
