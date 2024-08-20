13237d28f8ed7084468cccd862822f8504eae263
Craig White <gerzytet@gmail.com>
2024-08-20 12:23:40 -0400

    fix build

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Crate : SizeObject
